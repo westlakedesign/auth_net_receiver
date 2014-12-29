5d33c28a20632026881a4bf83f1781667c5381a3
Greg Woods <greg@westlakedesign.com>
2014-12-29 12:31:07 -0500

    remove use of deprecated :use_route option

--- spec/controllers/auth_net_receiver/raw_transactions_controller_spec.rb
+++ spec/controllers/auth_net_receiver/raw_transactions_controller_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 module AuthNetReceiver
   RSpec.describe RawTransactionsController, :type => :controller do
 
+    routes { AuthNetReceiver::Engine.routes }
+
     it "should create a new raw transaction" do
-      post :create, {:use_route => :auth_net_receiver}
+      post :create 
       expect(response).to be_success
       expect(AuthNetReceiver::RawTransaction.count).to eq(1)
     end
